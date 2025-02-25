024297d6122747815723841c84e31b777621567c
2533Aryan <2533aryan@gmail.com>
2025-02-25 18:13:49 -0500

    basic interface

--- dump-bot/src/App.tsx
+++ dump-bot/src/App.tsx
@@ -1,6 +1,16 @@
-import { useState } from 'react'
+import React from 'react'
 
 function App() {
+  return <main>
+    <h1>Thought-Dump-AI</h1>
+    <div>
+      <p>What's on your mind?</p>
+    </div>
+    <form>
+      <input type="text" placeholder='message' />
+      <input type="submit" value="send"/>
+    </form>
+  </main>
 }
 
 export default App
